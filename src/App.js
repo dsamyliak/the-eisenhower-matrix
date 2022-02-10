287f835b882e6cd0f13932c7af01d8f74ee045d8
Dmytro Samyliak <dsamyliak@gmail.com>
2022-02-10 12:52:22 +0200

    change colors and header of quarter-blocks

--- src/App.js
+++ src/App.js
@@ -8,29 +8,21 @@ class App extends React.Component {
       <>
         <div className="First-block">
           <ul className="First-quarter-list">
-            <h1>
-              1 <span>Urgent Important</span>
-            </h1>
+            <h1 className="Quarter-header">1. Urgent Important</h1>
             <li>Task</li>
           </ul>
           <ul className="Second-quarter-list">
-            <h1>
-              2 <span>Less Urgent Important</span>
-            </h1>
+            <h1 className="Quarter-header">2. Less Urgent Important</h1>
             <li>Task</li>
           </ul>
         </div>
         <div className="Second-block">
           <ul className="Third-quarter-list">
-            <h1>
-              3 <span>Urgent Less Important</span>
-            </h1>
+            <h1 className="Quarter-header">3. Urgent Less Important</h1>
             <li>Task</li>
           </ul>
           <ul className="Fourth-quarter-list">
-            <h1>
-              4 <span>Less Urgent Less Important</span>
-            </h1>
+            <h1 className="Quarter-header">4. Less Urgent Less Important</h1>
             <li>Task</li>
           </ul>
         </div>
